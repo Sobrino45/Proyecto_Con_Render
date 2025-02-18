38afb24c453b0b18e06117df054c74b478971c74
Sobrino45 <jesus.sobrino-sanchez@iesruizgijon.com>
2025-02-18 08:25:36 +0100

    Corrección de configuración de Git y actualización del proyecto

---
+++ src/main/java/com/example/demo/ProyectoConRenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoConRenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoConRenderApplication.class, args);
+	}
+
+}
